10fa2f74a94563bf6f93cb390572f8d7a4fcab61
guopeng <15837097057@163.com>
2019-03-17 11:47:05 +0800

    111

--- src/Test11.java
+++ src/Test11.java
@@ -1,15 +1,22 @@
+import java.io.UnsupportedEncodingException;
 import java.util.TreeMap;
 
 public class Test11 {
-    public static void main(String[] args) {
-        int i =0;
+    public static void main(String[] args) throws UnsupportedEncodingException {
+       /* int i =0;
         Object o = new Object();
         synchronized (o){
             for (int j = 0; j < 10; j++) {
                 i++;
                 System.out.println(i);
             }
+        }*/
+        String a = "abc";
+        byte[] bytes = a.getBytes("utf-8");
+        for (int j = 0; j < bytes.length; j++) {
+            System.out.println(bytes[j]);
         }
+
         new TreeMap();
     }
 }
